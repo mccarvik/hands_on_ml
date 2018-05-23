543d3a5e43da989c30175c59509247ab76f401aa
mccarvik <mccarviks@gmail.com>
2018-05-23 01:52:51 +0000

    some more kernel trick stuff

--- 5ch/supp_vector_mach.py
+++ 5ch/supp_vector_mach.py
@@ -5,7 +5,7 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn.svm import SVC, LinearSVC, LinearSVR
+from sklearn.svm import SVC, LinearSVC, LinearSVR, SVR
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.datasets import make_moons
@@ -49,6 +49,26 @@ def regression():
     plot_svm_regression(svm_reg2, X, y, [0, 2, 3, 11])
     plt.title(r"$\epsilon = {}$".format(svm_reg2.epsilon), fontsize=18)
     plt.savefig(PNG_PATH + "svm_regression_plot", dpi=300)
+    plt.close()
+    
+    m = 100
+    X = 2 * np.random.rand(m, 1) - 1
+    y = (0.2 + 0.1 * X + 0.5 * X**2 + np.random.randn(m, 1)/10).ravel()
+    
+    svm_poly_reg1 = SVR(kernel="poly", degree=2, C=100, epsilon=0.1)
+    svm_poly_reg2 = SVR(kernel="poly", degree=2, C=0.01, epsilon=0.1)
+    svm_poly_reg1.fit(X, y)
+    svm_poly_reg2.fit(X, y)
+    
+    plt.figure(figsize=(9, 4))
+    plt.subplot(121)
+    plot_svm_regression(svm_poly_reg1, X, y, [-1, 1, 0, 1])
+    plt.title(r"$degree={}, C={}, \epsilon = {}$".format(svm_poly_reg1.degree, svm_poly_reg1.C, svm_poly_reg1.epsilon), fontsize=18)
+    plt.ylabel(r"$y$", fontsize=18, rotation=0)
+    plt.subplot(122)
+    plot_svm_regression(svm_poly_reg2, X, y, [-1, 1, 0, 1])
+    plt.title(r"$degree={}, C={}, \epsilon = {}$".format(svm_poly_reg2.degree, svm_poly_reg2.C, svm_poly_reg2.epsilon), fontsize=18)
+    plt.savefig(PNG_PATH + "svm_with_polynomial_kernel_plot", dpi=300)
     plt.close()
     
 
