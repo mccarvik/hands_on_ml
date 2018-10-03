48be8242484661222b9dd924987c2490ab16db16
mccarvik <mccarviks@gmail.com>
2018-10-03 21:12:44 +0000

    more progress, getting there

--- 14ch/recurrent_nn.py
+++ 14ch/recurrent_nn.py
@@ -10,11 +10,58 @@ from functools import partial
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+from six.moves import urllib
+import errno
+import os
+import zipfile
+
 PNG_PATH = '/home/ubuntu/workspace/hands_on_ml/png/14ch/'
 
 
 def lstm():
-    pass
+    reset_graph()
+    n_steps = 28
+    n_inputs = 28
+    n_neurons = 150
+    n_outputs = 10
+    n_layers = 3
+    learning_rate = 0.001
+    
+    X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+    y = tf.placeholder(tf.int32, [None])
+    lstm_cells = [tf.contrib.rnn.BasicLSTMCell(num_units=n_neurons) for layer in range(n_layers)]
+    multi_cell = tf.contrib.rnn.MultiRNNCell(lstm_cells)
+    outputs, states = tf.nn.dynamic_rnn(multi_cell, X, dtype=tf.float32)
+    top_layer_h_state = states[-1][1]
+    logits = tf.layers.dense(top_layer_h_state, n_outputs, name="softmax")
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    training_op = optimizer.minimize(loss)
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+        
+    init = tf.global_variables_initializer()
+    print(states)
+    print(top_layer_h_state)
+    
+    n_epochs = 11
+    batch_size = 10
+    mnist = input_data.read_data_sets("/tmp/data/")
+    X_test = mnist.test.images.reshape((-1, n_steps, n_inputs))
+    y_test = mnist.test.labels
+    with tf.Session() as sess:
+        init.run()
+        for epoch in range(n_epochs):
+            for iteration in range(200 // batch_size):
+                X_batch, y_batch = mnist.train.next_batch(batch_size)
+                X_batch = X_batch.reshape((batch_size, n_steps, n_inputs))
+                sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+            acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+            acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
+            print("Epoch", epoch, "Train accuracy =", acc_train, "Test accuracy =", acc_test)
+    lstm_cell = tf.contrib.rnn.LSTMCell(num_units=n_neurons, use_peepholes=True)
+    gru_cell = tf.contrib.rnn.GRUCell(num_units=n_neurons)
 
 
 def dropout():
@@ -571,4 +618,5 @@ if __name__ == '__main__':
     # without_OPW()
     # multi_rnn_cell()
     # distribute_deep_rnn_across_gpu()
-    dropout()
+    # dropout()
+    lstm()
